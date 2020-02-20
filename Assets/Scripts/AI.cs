484ce59d3d735b88f407f3da92425c8f785254d2
gascoyne-c <35404901+gascoyne-c@users.noreply.github.com>
2020-02-19 20:39:23 -0500

    Add files via upload
    Scenes/Main Level should be what you need to open in Unity to load the project. Make sure to unzip the files into a folder somewhere locally just to make sure everything is there properly.

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    public Transform target;
+    public Transform myTransform;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+
+    Transform destination;
+    NavMeshAgent navMeshAgent;
+
+    public float speed;
+
+    void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+        }
+
+        else
+        {
+            SetDestination();
+        }
+
+    }
+
+    private void SetDestination()
+    {
+        if(destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+    void Update()
+    {
+        //transform.position = Vector2.MoveTowards(transform.position, destination.position, speed * Time.deltaTime);
+    }
+}
